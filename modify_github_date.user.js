db6c83fb16df5168fe13f65679bba6fd1b41865f
LonelyWindG <hai0394@outlook.com>
2025-06-17 17:39:26 +0800

    [Github日期转换] 调整日期转换规则, 优先匹配github现有格式, 未匹配到再按自定规则转换日期; 添加DOM变化监听, 动态修改刷新的日期;

--- modify_github_date.user.js
+++ modify_github_date.user.js
@@ -1,126 +1,148 @@
 // ==UserScript==
 // @name         Github日期转换
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Github日期转换
+// @version      2025-06-17
+// @description  将GitHub的日期显示转换为中文，规则尽量与GitHub一致
 // @author       Wind
 // @match        https://github.com/*
-// @icon         http://github.com/favicon.ico
-// @grant        GM_registerMenuCommand
+// @grant        none
 // ==/UserScript==
 
-GM_registerMenuCommand("[Github日期转换]强制执行", modifyGithubDate);
 modifyGithubDate();
-setTimeout(modifyGithubDate, 2000);
-listenClick();
 
-function modifyGithubDate(){
-    var relative_times = document.getElementsByTagName("relative-time");
-    console.log("[Github日期转换]相对时间标签数量: " + relative_times.length);
+function modifyGithubDate() {
+    'use strict';
 
-    var now = new Date();
-    var title_formatter = new Intl.DateTimeFormat('zh-CN', { dateStyle:"full", timeStyle:"full", timeZone: "Asia/Shanghai" });
-    var content_formatter0 = new Intl.DateTimeFormat('zh-CN', { dateStyle:"long", timeZone: "Asia/Shanghai" });
-    var content_formatter1 = new Intl.DateTimeFormat('zh-CN', { month:"long", day:"numeric", timeZone: "Asia/Shanghai" });
-    var relative_time_formatter = new Intl.RelativeTimeFormat('zh-CN', { numeric:"auto" });
-    for (var item of relative_times) {
-        if (item.getAttribute("modified") != null) continue;
-        if (item.shadowRoot == null) continue;
+    console.log('[Github日期转换] 脚本已加载');
 
-        item.setAttribute("modified", "1");
-        var date = item.date;
+    var title_formatter = new Intl.DateTimeFormat('zh-CN', { dateStyle: "full", timeStyle: "full", timeZone: "Asia/Shanghai" });
+    var content_formatter0 = new Intl.DateTimeFormat('zh-CN', { dateStyle: "long", timeZone: "Asia/Shanghai" });
+    var content_formatter1 = new Intl.DateTimeFormat('zh-CN', { month: "long", day: "numeric", timeZone: "Asia/Shanghai" });
+    var relative_time_formatter = new Intl.RelativeTimeFormat('zh-CN', { numeric: "auto" });
+
+    // 英文到中文的映射
+    const map = [
+        { re: /^now$/i, zh: '刚刚' },
+        { re: /^just now$/i, zh: '刚刚' },
+        { re: /^(\d+)\s*seconds?\s*ago$/i, zh: '$1秒前' },
+        { re: /^(\d+)\s*minutes?\s*ago$/i, zh: '$1分钟前' },
+        { re: /^(\d+)\s*hours?\s*ago$/i, zh: '$1小时前' },
+        { re: /^yesterday$/i, zh: '昨天' },
+        { re: /^(\d+)\s*days?\s*ago$/i, zh: '$1天前' },
+        { re: /^last?\s*week$/i, zh: '上周' },
+        { re: /^(\d+)\s*weeks?\s*ago$/i, zh: '$1周前' },
+        { re: /^last?\s*month$/i, zh: '上个月' },
+        { re: /^(\d+)\s*months?\s*ago$/i, zh: '$1个月前' },
+        { re: /^last?\s*year$/i, zh: '去年' },
+        { re: /^(\d+)\s*years?\s*ago$/i, zh: '$1年前' },
+    ];
+
+    function en2zh(text) {
+        for (const { re, zh } of map) {
+            if (re.test(text)) {
+                // console.log(`[Github日期转换] 匹配到: "${text}" => "${text.replace(re, zh)}"`);
+                return text.replace(re, zh);
+            }
+        }
+        return null;
+    }
+
+    function translateNode(node) {
+        if (!node || node.shadowRoot == null) return;
+
+        const content = node.shadowRoot.textContent;
+        if (content.startsWith('\u200b')) {
+            return;
+        }
+
+        var date = node.date;
         var localeTime = title_formatter.format(date);
-        item.title = localeTime;
-        // console.log("[Github日期转换]localeTime: " + localeTime.toString());
-        
-        var content = item.shadowRoot.textContent;
-        var always_relative_time = item.tense == "past";
-        if (date.getFullYear() != now.getFullYear())
-        {
-            if (always_relative_time)
-            {
+        node.title = localeTime;
+        var newContent = en2zh(content);
+
+        var always_relative_time = node.tense == "past";
+        if (newContent === null) {
+            // newContent = formatDate(date);
+            newContent = formatDateZh(date, always_relative_time);
+            console.log(`[Github日期转换] 格式化日期: "${content}" => "${newContent}"`);
+        }
+        node.shadowRoot.textContent = "\u200b" + newContent;
+    }
+
+    function formatDate(date) {
+        const now = new Date();
+        var content = "";
+        if (date.getFullYear() != now.getFullYear()) {
+            content = content_formatter0.format(date);
+        } else {
+            content = content_formatter1.format(date);
+        }
+        return content;
+    }
+
+    function formatDateZh(date, always_relative_time) {
+        const now = new Date();
+        var content = "";
+        if (date.getFullYear() != now.getFullYear()) {
+            if (always_relative_time) {
                 content = relative_time_formatter.format(date.getFullYear() - now.getFullYear(), "year");
             }
-            else
-            {
+            else {
                 content = content_formatter0.format(date);
             }
         }
-        else
-        {
-            if (date.getMonth() != now.getMonth())
-            {
-                if (always_relative_time)
-                {
+        else {
+            if (date.getMonth() != now.getMonth()) {
+                if (always_relative_time) {
                     content = relative_time_formatter.format(date.getMonth() - now.getMonth(), "month");
                 }
-                else
-                {
+                else {
                     content = content_formatter1.format(date);
                 }
             }
-            else if (date.getDate() != now.getDate())
-            {
+            else if (date.getDate() != now.getDate()) {
                 var dayDiff = date.getDate() - now.getDate();
-                if (dayDiff < -5)
-                {
+                if (dayDiff < -5) {
                     var dateWeek = getWeek(date);
                     var nowWeek = getWeek(now);
                     content = relative_time_formatter.format(dateWeek - nowWeek, "week");
                 }
-                else 
-                { 
+                else {
                     content = relative_time_formatter.format(dayDiff, "day");
                 }
             }
-            else if (date.getHours() != now.getHours())
-            {
+            else if (date.getHours() != now.getHours()) {
                 content = relative_time_formatter.format(date.getHours() - now.getHours(), "hour");
             }
-            else if (date.getMinutes() != now.getMinutes())
-            {
+            else if (date.getMinutes() != now.getMinutes()) {
                 content = relative_time_formatter.format(date.getMinutes() - now.getMinutes(), "minute");
             }
-            else
-            {
+            else {
                 content = relative_time_formatter.format(date.getSeconds() - now.getSeconds(), "second");
             }
         }
-
-        item.shadowRoot.textContent = content;
+        return content;
     }
-}
 
-function getWeek(date){
-    var weekDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-    var firstWeekDayInLastMouth = (weekDay == 0 ? 7 : weekDay) - 1;
-    return Math.ceil((firstWeekDayInLastMouth + date.getDate()) / 7);
-}
+    function getWeek(date) {
+        var weekDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+        var firstWeekDayInLastMouth = (weekDay == 0 ? 7 : weekDay) - 1;
+        return Math.ceil((firstWeekDayInLastMouth + date.getDate()) / 7);
+    }
 
-function listenClick(){
-    document.body.addEventListener("click", modifyAfterClick);
-    // var assets = document.getElementsByClassName("f3 text-bold d-inline mr-3");
-    // console.log(assets.length);
-    // for (var item of assets) {
-    //     item.removeEventListener('click', modifyAfterClick);
-    //     item.addEventListener('click', modifyAfterClick);
-    // }
+    function modifyAll() {
+        const elements = document.querySelectorAll('relative-time');
+        if (elements.length === 0) {
+            return;
+        }
+        // console.log(`[Github日期转换] 检测到DOM变化，重新转换, ${elements.length}个待转换元素`);
+        elements.forEach(translateNode);
+    }
 
-    // var tabs = document.getElementsByClassName("js-selected-navigation-item subnav-item");
-    // for (var item of tabs) {
-    //     item.removeEventListener('click', modifyAfterClick);
-    //     item.addEventListener('click', modifyAfterClick);
-    // }
-}
+    // 初始转换
+    modifyAll();
 
-function modifyAfterClick(event){
-    var target = event.target || event.srcElement;
-    var nodeName = target.nodeName.toLocaleLowerCase();
-    if (nodeName != "a" && nodeName != "span") return;
-
-    console.log("[Github日期转换]modifyAfterClick");
-    setTimeout(() =>{
-        modifyGithubDate();
-        listenClick();
-    }, 2000);
+    // 监听 DOM 变化，动态转换
+    const observer = new MutationObserver(modifyAll);
+    observer.observe(document.body, { childList: true, subtree: true });
 }
